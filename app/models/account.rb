0dcd7ff059f77948f5024e2cc147947e837aa411
Henrique Breim <henrique@breim.com.br>
2025-08-17 09:13:29 +0200

    feat: add account has_many relations

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class Account < ApplicationRecord
+  has_many :subscriptions, dependent: :destroy
+  has_many :products, through: :subscriptions
+  has_many :users, dependent: :destroy
+
   validates :name, presence: true
 end
