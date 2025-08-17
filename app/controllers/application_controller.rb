427ceb91c713ced2577e7c4dae77545a1939eb1d
Henrique Breim <henrique@breim.com.br>
2025-08-17 07:39:10 +0200

    chore: generate responders

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
+require 'application_responder'
+
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  self.responder = ApplicationResponder
+  respond_to :html
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
