19da5add2b0ac95a4b7796959b971663ea61c8b4
Henrique Breim <henrique@breim.com.br>
2025-08-17 08:14:40 +0200

    chore: configure root path of application

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'admin/accounts#index'
+  get 'admin', to: redirect('/admin/accounts')
+
   namespace :admin do
     resources :accounts
   end
