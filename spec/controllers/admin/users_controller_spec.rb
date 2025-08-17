838f2bb87ba2ce2341573354c630815c7e5231db
Henrique Breim <henrique@breim.com.br>
2025-08-17 08:37:46 +0200

    test: generate specs for users controllers

---
+++ spec/controllers/admin/users_controller_spec.rb
@@ -0,0 +1,179 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Admin::UsersController, type: :controller do
+  let(:account) { create(:account) }
+  let(:user) { create(:user, account: account) }
+  let(:valid_attributes) { { name: 'Test User', email: 'test@example.com', account_id: account.id } }
+  let(:invalid_attributes) { { name: '', email: 'test@example.com', account_id: account.id } }
+
+  describe 'GET #index' do
+    it 'assigns all users as @users' do
+      user1 = create(:user)
+      user2 = create(:user)
+      get :index
+      expect(assigns(:users)).to match_array([user1, user2])
+    end
+
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+
+    it 'returns a successful response' do
+      get :index
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET #show' do
+    it 'assigns the requested user as @user' do
+      get :show, params: { id: user.to_param }
+      expect(assigns(:user)).to eq(user)
+    end
+
+    it 'renders the show template' do
+      get :show, params: { id: user.to_param }
+      expect(response).to render_template(:show)
+    end
+
+    it 'returns a successful response' do
+      get :show, params: { id: user.to_param }
+      expect(response).to be_successful
+    end
+
+    it 'raises ActiveRecord::RecordNotFound for invalid id' do
+      expect do
+        get :show, params: { id: 'invalid' }
+      end.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'assigns the requested user as @user' do
+      get :edit, params: { id: user.to_param }
+      expect(assigns(:user)).to eq(user)
+    end
+
+    it 'renders the edit template' do
+      get :edit, params: { id: user.to_param }
+      expect(response).to render_template(:edit)
+    end
+
+    it 'returns a successful response' do
+      get :edit, params: { id: user.to_param }
+      expect(response).to be_successful
+    end
+
+    it 'raises ActiveRecord::RecordNotFound for invalid id' do
+      expect do
+        get :edit, params: { id: 'invalid' }
+      end.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'with valid parameters' do
+      let(:new_attributes) { { name: 'Updated User Name', email: 'updated@example.com' } }
+
+      it 'updates the requested user' do
+        patch :update, params: { id: user.to_param, user: new_attributes }
+        user.reload
+        expect(user.name).to eq('Updated User Name')
+        expect(user.email).to eq('updated@example.com')
+      end
+
+      it 'assigns the user as @user' do
+        patch :update, params: { id: user.to_param, user: new_attributes }
+        expect(assigns(:user)).to eq(user)
+      end
+
+      it 'redirects to the user' do
+        patch :update, params: { id: user.to_param, user: new_attributes }
+        expect(response).to redirect_to(admin_user_path(user))
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'does not update the user' do
+        original_name = user.name
+        patch :update, params: { id: user.to_param, user: invalid_attributes }
+        user.reload
+        expect(user.name).to eq(original_name)
+      end
+
+      it 'assigns the user as @user' do
+        patch :update, params: { id: user.to_param, user: invalid_attributes }
+        expect(assigns(:user)).to eq(user)
+      end
+
+      it 'renders the edit template' do
+        patch :update, params: { id: user.to_param, user: invalid_attributes }
+        expect(response).to render_template(:edit)
+      end
+    end
+
+    it 'raises ActiveRecord::RecordNotFound for invalid id' do
+      expect do
+        patch :update, params: { id: 'invalid', user: valid_attributes }
+      end.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  describe 'GET #new' do
+    it 'assigns a new user as @user' do
+      get :new
+      expect(assigns(:user)).to be_a_new(User)
+    end
+
+    it 'renders the new template' do
+      get :new
+      expect(response).to render_template(:new)
+    end
+
+    it 'returns a successful response' do
+      get :new
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid parameters' do
+      it 'creates a new User' do
+        expect do
+          post :create, params: { user: valid_attributes }
+        end.to change(User, :count).by(1)
+      end
+
+      it 'assigns a newly created user as @user' do
+        post :create, params: { user: valid_attributes }
+        expect(assigns(:user)).to be_a(User)
+        expect(assigns(:user)).to be_persisted
+      end
+
+      it 'redirects to the created user' do
+        post :create, params: { user: valid_attributes }
+        expect(response).to redirect_to(admin_user_path(User.last))
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'does not create a new User' do
+        expect do
+          post :create, params: { user: invalid_attributes }
+        end.not_to change(User, :count)
+      end
+
+      it 'assigns a newly created but unsaved user as @user' do
+        post :create, params: { user: invalid_attributes }
+        expect(assigns(:user)).to be_a_new(User)
+      end
+
+      it 'renders the new template' do
+        post :create, params: { user: invalid_attributes }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+end
